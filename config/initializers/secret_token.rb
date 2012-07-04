82aa883bbf4d6aa1ad5dc365c269e0ffcde2f092
Marc Tauber <marc@bangoid.com>
2012-07-04 23:02:37 +0100

    Initial commit of AA

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aa::Application.config.secret_token = '3765177f501db97f1af0fbf9a0f3e4fec9acdb076387216edb8681b8b36e769d599b849cd814c7387d3b3bcb5c43cea6ebddf57072ece1b542e4ef1cb1659c6b'
